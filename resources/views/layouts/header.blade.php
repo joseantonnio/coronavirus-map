861fc274b857c8b978165492836ada9a6d448af8
José Antonio <jasj_brasil@hotmail.com>
2020-03-15 23:50:46 -0300

    Adds favicons and fixes viewport

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -3,6 +3,8 @@
 
 <head>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Primary Meta Tags -->
     <title>Mapa do Coronavírus no Brasil - Conheça as cidades afetadas</title>
@@ -37,14 +39,13 @@
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.0/themes/smoothness/jquery-ui.css" />
 
     <!-- Favicons -->
-    <link rel="apple-touch-icon" href="images/favicons/apple-touch-icon.png" sizes="180x180">
-    <link rel="icon" href="images/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
-    <link rel="icon" href="images/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
-    <link rel="manifest" href="images/favicons/manifest.json">
-    <link rel="mask-icon" href="images/favicons/safari-pinned-tab.svg" color="#563d7c">
-    <link rel="icon" href="images/favicons/favicon.ico">
-    <meta name="msapplication-config" content="images/favicons/browserconfig.xml">
-    <meta name="theme-color" content="#563d7c">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('site.webmanifest') }}">
+    <link rel="mask-icon" href="{{ asset('images/safari-pinned-tab.svg') }}" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#ffc40d">
+    <meta name="theme-color" content="#272b30">
 
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
