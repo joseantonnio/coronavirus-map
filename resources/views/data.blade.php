6fcbe469484ea7ed1f04c03a2b73e028d7debc4c
José Antonio <jose.antonio@performmaonline.com>
2020-03-18 02:18:11 -0300

    Enhanced data page

--- resources/views/data.blade.php
+++ resources/views/data.blade.php
@@ -7,44 +7,97 @@
 
 @section('content')
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Tabela de Dados</h1>
-        <div class="btn-toolbar mb-2 mb-md-0">
-            <div class="btn-group mr-2">
-                <button type="button" class="btn btn-sm btn-outline-secondary" data-toggle="modal" data-target=".modal-share"><span data-feather="share"></span> Compartilhar</button>
-            </div>
-        </div>
+        <h1 class="h2">Tabelas de Dados</h1>
     </div>
 
     <div class="table-responsive pt-3 pb-2 mb-3">
         <table class="table table-striped">
             <thead class="thead-dark">
                 <tr>
-                    <th scope="col">Cidade</th>
-                    <th scope="col">Casos</th>
-                    <th scope="col">Casos Graves (UTI)</th>
-                    <th scope="col">Recuperados</th>
-                    <th scope="col">Mortes</th>
-                    <th scope="col">Data do Primeiro Caso</th>
+                    <th colspan="6" class="text-center">Informações por Estado</th>
                 </tr>
             </thead>
             <tbody>
-                @if ($data->count() < 1)
+                <tr class="bg-secondary text-white">
+                    <td>Estado</td>
+                    <td>UF</td>
+                    <td>Casos</td>
+                    <td>Casos Graves (UTI)</td>
+                    <td>Recuperados</td>
+                    <td>Mortes</td>
+                </tr>
+                @if ($states->count() < 1)
                     <tr>
                         <td class="text-center" colspan="6">Não há dados para serem exibidos...</td>
                     </tr>
                 @endif
 
-                @foreach ($data as $infection)
+                @foreach ($states as $state)
                     <tr>
-                        <td>{{ $infection[0] }}</td>
-                        <td>{{ $infection[1] }}</td>
-                        <td>{{ $infection[2] }}</td>
-                        <td>{{ $infection[3] }}</td>
-                        <td>{{ $infection[4] }}</td>
-                        <td>{{ $infection[5] }}</td>
+                        <td>{{ $state->name }}</td>
+                        <td>{{ $state->uf }}</td>
+                        <td>{{ is_null($state->total_cases) ? 0 : $state->total_cases }}</td>
+                        <td>{{ is_null($state->total_serious) ? 0 : $state->total_serious }}</td>
+                        <td>{{ is_null($state->total_recovered) ? 0 : $state->total_recovered }}</td>
+                        <td>{{ is_null($state->total_deaths) ? 0 : $state->total_deaths }}</td>
                     </tr>
                 @endforeach
             </tbody>
+            <tfoot>
+                <tr class="bg-secondary text-white">
+                    <td colspan="2"></td>
+                    <td>{{ $states->sum('total_cases') }}</td>
+                    <td>{{ $states->sum('total_serious') }}</td>
+                    <td>{{ $states->sum('total_recovered') }}</td>
+                    <td>{{ $states->sum('total_deaths') }}</td>
+                </tr>
+            </tfoot>
+        </table>
+    </div>
+
+    <div class="table-responsive pt-3 pb-2 mb-3">
+        <table class="table table-striped">
+            <thead class="thead-dark">
+                <tr>
+                    <th colspan="6" class="text-center">Informações por Município</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="bg-secondary text-white">
+                    <td>Cidade</td>
+                    <td>Casos</td>
+                    <td>Casos Graves (UTI)</td>
+                    <td>Recuperados</td>
+                    <td>Mortes</td>
+                    <td>Data do Primeiro Caso</td>
+                </tr>
+                @if ($infections->count() < 1)
+                    <tr>
+                        <td class="text-center" colspan="6">Não há dados para serem exibidos...</td>
+                    </tr>
+                @endif
+
+                @foreach ($infections as $infection)
+                    <tr>
+                        <td>{{ $infection->city->name . ', ' . $infection->city->state->uf }}</td>
+                        <td>{{ $infection->cases }}</td>
+                        <td>{{ $infection->serious }}</td>
+                        <td>{{ $infection->recovered }}</td>
+                        <td>{{ $infection->deaths }}</td>
+                        <td>{{ $infection->first_case->translatedFormat('d \d\e F \d\e Y') }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+            <tfoot>
+                <tr class="bg-secondary text-white">
+                    <td></td>
+                    <td>{{ $infections->sum('cases') }}</td>
+                    <td>{{ $infections->sum('serious') }}</td>
+                    <td>{{ $infections->sum('recovered') }}</td>
+                    <td>{{ $infections->sum('deaths') }}</td>
+                    <td></td>
+                </tr>
+            </tfoot>
         </table>
     </div>
 @endsection
